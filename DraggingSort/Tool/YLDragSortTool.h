03f11af82b7768075ebbd4696618f7d2d0c9fa4d
HelloYeah <Sekorm@HelloYeah.local>
2016-12-05 17:28:15 +0800

    添加文件

---
+++ DraggingSort/Tool/YLDragSortTool.h
@@ -0,0 +1,15 @@
+//
+//  SKSubscribeDragSortTool.h
+//  Sinfo
+//
+//  Created by Sekorm on 2016/11/30.
+//  Copyright © 2016年 YeLiang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YLDragSortTool : NSObject
+@property (nonatomic,assign) BOOL isEditing;
+@property (nonatomic,strong) NSMutableArray * subscribeArray;
++ (instancetype)shareInstance;
+@end
---
+++ DraggingSort/Tool/YLDragSortTool.m
@@ -0,0 +1,54 @@
+//
+//  SKDragSortTool.m
+//  Sinfo
+//
+//  Created by Sekorm on 2016/11/30.
+//  Copyright © 2016年 YeLiang. All rights reserved.
+//
+
+#import "YLDragSortTool.h"
+
+@implementation YLDragSortTool
+static YLDragSortTool *DragSortTool = nil;
+
++ (instancetype)shareInstance
+{
+    @synchronized (self) {
+        if (DragSortTool == nil) {
+            DragSortTool = [[self alloc] init];
+        }
+    }
+    
+    return DragSortTool;
+}
+
++ (instancetype)allocWithZone:(struct _NSZone *)zone
+{
+    @synchronized (self) {
+        if (DragSortTool == nil) {
+            DragSortTool = [super allocWithZone:zone];
+        }
+    }
+    return DragSortTool;
+}
+
+- (id)copy
+{
+    return DragSortTool;
+}
+
+- (id)mutableCopy{
+    return DragSortTool;
+}
+
+- (NSMutableArray *)subscribeArray {
+
+    if (!_subscribeArray) {
+        
+        _subscribeArray = [@[@"推荐",@"视频",@"军事",@"娱乐",@"问答",@"娱乐",@"汽车",@"段子",@"趣图",@"财经",@"热点",@"房产",@"社会",@"数码",@"美女",@"数码",@"文化",@"美文",@"星座",@"旅游",@"视频",@"军事",@"娱乐",@"问答",@"娱乐",@"汽车",@"段子",@"趣图",@"财经",@"热点",@"房产",@"社会",@"数码",@"美女",@"数码",@"文化",@"美文",@"星座",@"旅游"] mutableCopy];
+    }
+    return _subscribeArray;
+}
+
+
+@end
